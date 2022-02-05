75ddd88cd34e3bdabf559e7e5ef4c64c30d2c103
varunkrishna711 <varunkrishnachess@gmail.com>
2022-02-05 11:17:20 +0530

    initial stage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  const [pok, setPok] = useState("");
+  const [pokchosen, setPokchosen] = useState(false);
+  const [info, setInfo] = useState({
+            name: "", 
+            species: "", 
+            img: "",
+            hp: "",
+            attack: "",
+            defense: "",
+            type: "",
+  });
+
+  const searchPok = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pok}`)
+          .then((res) => {setInfo({
+            name: pok, 
+            species: res.data.species.name, 
+            img: res.data.sprites.other.dream_world.front_default, 
+            hp: res.data.stats[0].base_stat,
+            attack: res.data.stats[1].base_stat,
+            defense: res.data.stats[1].base_stat,
+            type: res.data.types[0].type.name,
+          });
+       }  
+      );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Pokemon Stats</h1>
+        <div className="form-group">
+          <label htmlFor="usr">Search for Pokemon:</label>
+          <input type="text" className="form-control" id="usr" onChange={(e) => setPok(e.target.value)} />
+        </div>
+        <button className="btn btn-warning" onClick={searchPok}>Submit</button>
+      </div>
+
+      <div className="displayContainer">
+        {
+              (pok!==info.name) ?
+              ( 
+              <>
+              <h1> {info.name}</h1>
+              <img src={info.img} alt={info.name} />
+              <h2>{info.species}</h2>
+              <h3>HP: {info.hp}</h3>
+              <h3>Attack: {info.attack}</h3>
+              <h3>Defense: {info.defense}</h3>
+              <h3>Type: {info.type}</h3>
+              </>
+              ) :null
+             
+        }
+      </div>
+    </>
   );
 }
 
